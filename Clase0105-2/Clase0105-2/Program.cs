07071b30b80a27f24544d4f338523c6c4f327047
Fernando Vicente <3021894750101@ingenieria.usac.edu.gt>
2025-05-01 07:59:09 -0600

    Ejemplo while con condición

---
+++ Clase0105-2/Clase0105-2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clase0105_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Realizar un programa que se ejecute hasta ingresar un
+            //número negativo
+            int numero = 0;
+
+            while(numero>=0)
+            {
+                Console.WriteLine("Ingrese un número: ");
+                numero = int.Parse(Console.ReadLine());
+                Console.WriteLine("Ingreso el número: " +  numero);
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Clase0105/Clase0105/Program.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clase0105
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Ejecutar un programa hasta que ingrese un número negativo
+            int numero = 0;
+            
+            while(numero >=0)
+            {
+                
+            }
+            Console.ReadKey();
+        }
+    }
+}
