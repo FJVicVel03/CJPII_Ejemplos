bdec29ce3ab65ec72d759525dd83883ab9d5e27b
Fernando Vicente <3021894750101@ingenieria.usac.edu.gt>
2025-05-15 07:35:21 -0600

    Ejemplo numero secreto

---
+++ Clase1505/Clase1505/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clase1505
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i=02;
+            int intentos = 1;
+            int numeroIngresado=0;
+
+            Random rnd = new Random();
+            int numero = rnd.Next(1, 100);  
+            
+
+            while(numeroIngresado!=numero)
+            {
+                Console.WriteLine("Ingrese el número: ");
+                numeroIngresado = int.Parse(Console.ReadLine());
+
+                if (numeroIngresado > numero)
+                {
+                    Console.WriteLine("Pruebe un número menor");
+                    i++;
+                }
+                else if (numeroIngresado < numero)
+                {
+                    Console.WriteLine("Pruebe un número mayor");
+                    i++;
+                } else if (numeroIngresado == numero)
+                {
+                    Console.WriteLine("Adivinó el número");
+                    Console.WriteLine("Intentos: " + i);
+                }
+                
+            }
+            Console.ReadLine();
+        }
+    }
+}
